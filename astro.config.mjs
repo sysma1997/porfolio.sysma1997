0bcc31aa834729266415b38ddfce5bc1002511ab
Sebastian Moreno Acero <sysma1997@gmail.com>
2026-07-02 23:31:21 -0500

    Upgrade project with new information, added translate, also added theme light and dark

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,12 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     build: {
         assets: "assets"
+    },
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en", "es"],
+        routing: {
+            prefixDefaultLocale: false
+        }
     }
 });
